c6e602a78212dc1caefcc10bd6f65f4837746089
sdeby <sdeby@hotmail.com>
2022-06-06 13:42:30 -0700

    chore: update repo

--- stacks/pipeline-stack.ts
+++ stacks/pipeline-stack.ts
@@ -1,26 +1,40 @@
-import { aws_codestarconnections as codeStartConnections, pipelines, Stack, StackProps, Stage } from "aws-cdk-lib"
-import { Construct } from "constructs"
+import {
+  aws_codestarconnections as codeStartConnections,
+  pipelines,
+  Stack,
+  StackProps,
+  Stage,
+} from 'aws-cdk-lib'
+import { Construct } from 'constructs'
 
 interface PipelineStackProps extends StackProps {
-  stages: Stage[],
-  accounts: Record<string, string>,
+  stages: Stage[]
+  accounts: Record<string, string>
 }
 
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props: PipelineStackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    const codeStarConnection = new codeStartConnections.CfnConnection(this, 'CodeStarConnection', {
-      connectionName: 'GitHubConnection',
-      providerType: 'GitHub'
-    })
+    const codeStarConnection = new codeStartConnections.CfnConnection(
+      this,
+      'CodeStarConnection',
+      {
+        connectionName: 'GitHubConnection',
+        providerType: 'GitHub',
+      }
+    )
 
-    const cdkContextArgs = Object.entries(props.accounts).map(([key, value]) => `-c ${key}=${value}`).join(' ')
+    const cdkContextArgs = Object.entries(props.accounts)
+      .map(([key, value]) => `-c ${key}=${value}`)
+      .join(' ')
 
+    const REPO = 'sdebylabs/cross-account-eventbridge'
+    // const repo = this.node.tryGetContext(`repo`)
     const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
       crossAccountKeys: true,
       synth: new pipelines.ShellStep('Synth', {
-        input: pipelines.CodePipelineSource.connection('fourTheorem/cross-account-eventbridge', 'main', {
+        input: pipelines.CodePipelineSource.connection(REPO, 'main', {
           connectionArn: codeStarConnection.ref,
           triggerOnPush: false, // TODO - set to true when working
         }),
