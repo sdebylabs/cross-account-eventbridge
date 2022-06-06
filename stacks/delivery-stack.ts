f4b72a8d0602ef9a32e7be2d38d59ae94b8659fb
sdeby <sdeby@hotmail.com>
2022-06-06 13:35:50 -0700

    feat: upgrade to NodeJS 16

--- stacks/delivery-stack.ts
+++ stacks/delivery-stack.ts
@@ -1,38 +1,43 @@
-import { Construct } from 'constructs';
+import { Construct } from 'constructs'
 import * as events from 'aws-cdk-lib/aws-events'
 import { LambdaFunction as LambdaFunctionTarget } from 'aws-cdk-lib/aws-events-targets'
 import * as nodeLambda from 'aws-cdk-lib/aws-lambda-nodejs'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { BaseStack, BaseStackProps } from './base-stack'
-import { Duration } from 'aws-cdk-lib';
-import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { Duration } from 'aws-cdk-lib'
+import { RetentionDays } from 'aws-cdk-lib/aws-logs'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
 
 /**
  * Application to handle deliveries for orders
- * 
+ *
  * When an Order.Created event is received, this application "delivers" the order
  */
 export class DeliveryServiceStack extends BaseStack {
-
   localBus: events.EventBus
 
   constructor(scope: Construct, id: string, props: BaseStackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     this.createOrderDeliveryFunction()
   }
 
   createOrderDeliveryFunction() {
-    const orderDeliveryFunction = new nodeLambda.NodejsFunction(this, 'OrderDeliveryFunction', {
-      entry: './src/delivery-handler.ts',
-      handler: 'handleOrderCreated',
-      environment: {
-        BUS_ARN: this.globalBus.eventBusArn,
-      },
-      timeout: Duration.seconds(10),
-      logRetention: RetentionDays.ONE_WEEK,
-      tracing: lambda.Tracing.ACTIVE,
-    })
+    const orderDeliveryFunction = new nodeLambda.NodejsFunction(
+      this,
+      'OrderDeliveryFunction',
+      {
+        entry: './src/delivery-handler.ts',
+        handler: 'handleOrderCreated',
+        runtime: Runtime.NODEJS_16_X,
+        environment: {
+          BUS_ARN: this.globalBus.eventBusArn,
+        },
+        timeout: Duration.seconds(10),
+        logRetention: RetentionDays.ONE_WEEK,
+        tracing: lambda.Tracing.ACTIVE,
+      }
+    )
     orderDeliveryFunction.addToRolePolicy(this.globalBusPutEventsStatement)
 
     // The delivery function reacts to orders being created
--- stacks/order-stack.ts
+++ stacks/order-stack.ts
@@ -1,15 +1,16 @@
-import { Construct } from 'constructs';
+import { Construct } from 'constructs'
 import * as events from 'aws-cdk-lib/aws-events'
 import { LambdaFunction as LambdaFunctionTarget } from 'aws-cdk-lib/aws-events-targets'
 import * as nodeLambda from 'aws-cdk-lib/aws-lambda-nodejs'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as apigw from 'aws-cdk-lib/aws-apigateway'
-import { BaseStack, BaseStackProps } from './base-stack';
-import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { BaseStack, BaseStackProps } from './base-stack'
+import { RetentionDays } from 'aws-cdk-lib/aws-logs'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
 
 /**
  * Application to manage customer order
- * 
+ *
  * An HTTP endpoint is created to receive orders from customers.
  * When an order is created, an Order.Created event is sent.
  * When a delivery update is received, the order is updated and an Order.Updated event is sent.
@@ -18,37 +19,47 @@ export class OrderServiceStack extends BaseStack {
   localBus: events.EventBus
 
   constructor(scope: Construct, id: string, props: BaseStackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     this.createOrderCreateFunction()
     this.createDeliveryUpdateFunction()
   }
 
   createOrderCreateFunction() {
-    const createOrderFunction = new nodeLambda.NodejsFunction(this, 'CreateOrderFunction', {
-      entry: './src/order-handler.ts',
-      handler: 'handleOrderCreate',
-      environment: {
-        BUS_ARN: this.globalBus.eventBusArn
-      },
-      logRetention: RetentionDays.ONE_WEEK,
-      tracing: lambda.Tracing.ACTIVE,
-    })
+    const createOrderFunction = new nodeLambda.NodejsFunction(
+      this,
+      'CreateOrderFunction',
+      {
+        entry: './src/order-handler.ts',
+        handler: 'handleOrderCreate',
+        runtime: Runtime.NODEJS_16_X,
+        environment: {
+          BUS_ARN: this.globalBus.eventBusArn,
+        },
+        logRetention: RetentionDays.ONE_WEEK,
+        tracing: lambda.Tracing.ACTIVE,
+      }
+    )
     createOrderFunction.addToRolePolicy(this.globalBusPutEventsStatement)
     const api = new apigw.RestApi(this, 'OrderApi', { restApiName: 'order' })
     api.root.addMethod('POST', new apigw.LambdaIntegration(createOrderFunction))
   }
 
   createDeliveryUpdateFunction() {
-    const deliveryUpdateFunction = new nodeLambda.NodejsFunction(this, 'DeliveryUpdateFunction', {
-      entry: './src/order-handler.ts',
-      handler: 'handleDeliveryUpdate',
-      environment: {
-        BUS_ARN: this.globalBus.eventBusArn
-      },
-      logRetention: RetentionDays.ONE_WEEK,
-      tracing: lambda.Tracing.ACTIVE,
-    })
+    const deliveryUpdateFunction = new nodeLambda.NodejsFunction(
+      this,
+      'DeliveryUpdateFunction',
+      {
+        entry: './src/order-handler.ts',
+        handler: 'handleDeliveryUpdate',
+        runtime: Runtime.NODEJS_16_X,
+        environment: {
+          BUS_ARN: this.globalBus.eventBusArn,
+        },
+        logRetention: RetentionDays.ONE_WEEK,
+        tracing: lambda.Tracing.ACTIVE,
+      }
+    )
     deliveryUpdateFunction.addToRolePolicy(this.globalBusPutEventsStatement)
 
     // React to delivery events
@@ -57,7 +68,7 @@ export class OrderServiceStack extends BaseStack {
       ruleName: 'order-service-rule',
       eventPattern: {
         detailType: ['Delivery.Updated'],
-      }
+      },
     }).addTarget(new LambdaFunctionTarget(deliveryUpdateFunction))
   }
 }
